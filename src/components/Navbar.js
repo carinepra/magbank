c0ce2da5d3d8765021c384e270c2138006b55859
Carine Carvalho <carinepradecarvalho@gmail.com>
2021-05-08 10:34:14 -0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Navbar.scss";
+
+import { Navbar, Nav, ButtonGroup, Button, NavDropdown, Container } from "react-bootstrap";
+
+const Navigation = () =>  (
+    <Navbar variant="dark" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">MAGBANK</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#cartao">Cartão</Nav.Link>
+                    <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                </Nav>
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="outline-light">
+                        <NavDropdown title="Acessa minha conta" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action">
+                                Action
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Button>
+                    <Button variant="outline-light">Abra sua conta</Button>
+                </ButtonGroup>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
+
+export default Navigation;
